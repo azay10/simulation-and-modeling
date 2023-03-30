3d757e0a507066cd72fd984172eedd04ba31994c
azay10 <117178019+azay10@users.noreply.github.com>
2023-03-30 07:05:07 +0545

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine(a);
+            float f = 5.75F;
+            Console.WriteLine(f);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace netProject
+{
+    internal class person
+    {
+        public person(int NoofEyes, int Height, string FaceColor, string Name)
+        {
+            noofEyes = NoofEyes;
+            height = Height;
+            faceColor = FaceColor;
+            name = Name;
+        }
+         ~person() {
+            Console.WriteLine("writing from destructor");
+            Console.Read();
+
+        }
+        public int noofEyes { get; set; }
+       public int height { get; set; }
+        public string faceColor { get; set; }
+        public string name { get; set; }
+       public void Lunch()
+        {
+            Console.WriteLine("I usually have momo in lunch");
+           
+        }
+        public void MyColor()
+        {
+            Console.WriteLine("My face color is" + faceColor);
+        }
+
+
+    
+
+        
+    }
+}
